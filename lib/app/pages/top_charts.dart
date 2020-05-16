bb73b31e6e9036ccaa57e80039835a05b2d1c313
401ZZ <pluto401zz@gmail.com>
2020-05-16 12:04:56 +0800

    修改排行榜页面

--- lib/app/pages/top_charts.dart
+++ lib/app/pages/top_charts.dart
@@ -8,106 +8,66 @@ class TopChartsPage extends StatefulWidget {
 class _TopChartsPageState extends State<TopChartsPage>
     with SingleTickerProviderStateMixin {
   TabController _controller;
+  double rpx;
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _controller = TabController(length: 2, vsync: this);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    double rpx = MediaQuery.of(context).size.width / 750;
-    double indicatorWidth = MediaQuery.of(context).size.width / 2 - 30;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('排行榜', style: TextStyle(fontSize: 36*rpx),),
-        centerTitle: true,
-        elevation: 0,
-        bottom: TabBar(
-          controller: _controller,
-          labelPadding: EdgeInsets.only(bottom: 10 * rpx),
-          labelColor: Colors.lightBlue,
-          unselectedLabelColor: Colors.black54,
-          labelStyle:
-              TextStyle(fontSize: 28 * rpx, fontWeight: FontWeight.bold),
-          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
-          indicatorPadding: EdgeInsets.only(
-              left: indicatorWidth * rpx, right: indicatorWidth * rpx),
-          tabs: <Widget>[
-            Text('男生频道'),
-            Text('女生频道'),
-          ],
-        ),
-      ),
-      body: SortPageBody(_controller),
-    );
-  }
-}
-
-class SortPageBody extends StatelessWidget {
-  TabController controller;
-
-  SortPageBody(this.controller);
-
-  @override
-  Widget build(BuildContext context) {
-    return TabBarView(
-      controller: controller,
-      children: <Widget>[
-        SortPageContainer(),
-        SortPageContainer(),
-      ],
-    );
-  }
-}
-
-class SortData {
-  String sortName;
-  List sortList = [];
-
-  SortData({this.sortName, this.sortList});
-}
-
-class BookDetail {
-  int bookId = 1;
-  String bookImg = 'images/OIP.jpg';
-  String bookTitle = '无题';
-  String author = '佚名';
-  String categoryName = '传说';
-  String bookDesc =
-      '是十九世纪和手机号就是时间合适就合适是十九世纪和手机号就是时间合适就合适是十九世纪和手机号就是时间合适就合适是十九世纪和手机号就是时间合适就合适';
-}
-
-class SortListData extends SortData {
-  BookDetail data = BookDetail();
-
-  @override
-  List get sortList => List.generate(5, (index) => sortList..add(data));
-  @override
-  String get sortName => sortName = '人气榜';
-}
-
-class SortPageContainer extends StatefulWidget {
-  @override
-  _SortPageContainerState createState() => _SortPageContainerState();
-}
-
-class _SortPageContainerState extends State<SortPageContainer> {
-  int indexItem; 
+  int indexItem;
   List<SortListData> sortLists = [];
   SortListData data = SortListData();
 
   @override
   void initState() {
     super.initState();
+    _controller = TabController(length: 2, vsync: this);
     indexItem = 0;
     List.generate(3, (index) => sortLists.add(data));
   }
 
-  Widget _getNavListItem(int index, rpx) {
-    Color bgColor = Color(0xfff4f4f4);  // 默认背景颜色
+  /// tabview
+  Widget _tabViewWidget() {
+    return TabBarView(
+      controller: _controller,
+      children: <Widget>[
+        _tabViewContentWidget(),
+        _tabViewContentWidget(),
+      ],
+    );
+  }
+
+  /// 排行榜页面内容
+  Widget _tabViewContentWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          flex: 2,
+          child: Container(
+            color: Color(0xfff4f4f4),
+            child: ListView.builder(
+              itemCount: sortLists.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _getNavListItem(index);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 7,
+          child: Container(
+            // margin: EdgeInsets.symmetric(horizontal:15*rpx),
+            alignment: Alignment.topCenter,
+            child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (BuildContext context, int index) {
+                  return _getListItem(index);
+                }),
+          ),
+        )
+      ],
+    );
+  }
+  // 排行榜分类菜单列表
+  Widget _getNavListItem(int index) {
+    Color bgColor = Color(0xfff4f4f4); // 默认背景颜色
     Color textColor = Colors.black54; // 默认文字颜色
     Color borderColor = Colors.transparent; // 默认左边框颜色
 
@@ -140,42 +100,8 @@ class _SortPageContainerState extends State<SortPageContainer> {
       ),
     );
   }
-
-  @override
-  Widget build(BuildContext context) {
-    double rpx = MediaQuery.of(context).size.width / 750;
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Color(0xfff4f4f4),
-            child: ListView.builder(
-              itemCount: sortLists.length,
-              itemBuilder: (BuildContext context, int index) {
-                return _getNavListItem(index, rpx);
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          flex: 7,
-          child: Container(
-            // margin: EdgeInsets.symmetric(horizontal:15*rpx),
-            alignment: Alignment.topCenter,
-            child: ListView.builder(
-                itemCount: 8,
-                itemBuilder: (BuildContext context, int index) {
-                  return _getListItem(index, rpx);
-                }),
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget _getListItem(index, rpx) {
+  // 排行榜分类列表
+  Widget _getListItem(int index) {
     int topNum = 0;
     List<Color> flagColors = [];
     switch (index) {
@@ -224,12 +150,10 @@ class _SortPageContainerState extends State<SortPageContainer> {
                       width: 35 * rpx,
                       padding: EdgeInsets.symmetric(vertical: 10 * rpx),
                       decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: flagColors,
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight
-                        )
-                      ),
+                          gradient: LinearGradient(
+                              colors: flagColors,
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight)),
                       child: Text(
                         '$topNum',
                         style: TextStyle(color: Colors.white),
@@ -240,7 +164,9 @@ class _SortPageContainerState extends State<SortPageContainer> {
                 ],
               ),
             ),
-            SizedBox(width: 20*rpx,),
+            SizedBox(
+              width: 20 * rpx,
+            ),
             Expanded(
               flex: 5,
               child: Column(
@@ -253,7 +179,7 @@ class _SortPageContainerState extends State<SortPageContainer> {
                   ),
                   Text(
                     '只是介绍，不超过2两行只是介绍，不超过2两行只是介绍，不超过2两行',
-                    style: TextStyle(fontSize: 26*rpx, color: Colors.grey),
+                    style: TextStyle(fontSize: 26 * rpx, color: Colors.grey),
                     maxLines: 2,
                     overflow: TextOverflow.ellipsis,
                   ),
@@ -273,8 +199,12 @@ class _SortPageContainerState extends State<SortPageContainer> {
                         ),
                       ),
                       Container(
-                        padding: EdgeInsets.only(right:10*rpx),
-                        child: Text('210万人气', style: TextStyle(color: Colors.deepOrange, fontSize: 20*rpx),),
+                        padding: EdgeInsets.only(right: 10 * rpx),
+                        child: Text(
+                          '210万人气',
+                          style: TextStyle(
+                              color: Colors.deepOrange, fontSize: 20 * rpx),
+                        ),
                       )
                     ],
                   )
@@ -286,4 +216,62 @@ class _SortPageContainerState extends State<SortPageContainer> {
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    rpx = MediaQuery.of(context).size.width / 750;
+    double indicatorWidth = MediaQuery.of(context).size.width / 2 - 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '排行榜',
+          style: TextStyle(fontSize: 36 * rpx),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        bottom: TabBar(
+          controller: _controller,
+          labelPadding: EdgeInsets.only(bottom: 10 * rpx),
+          labelColor: Colors.lightBlue,
+          unselectedLabelColor: Colors.black54,
+          labelStyle:
+              TextStyle(fontSize: 28 * rpx, fontWeight: FontWeight.bold),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.normal),
+          indicatorPadding: EdgeInsets.only(
+              left: indicatorWidth * rpx, right: indicatorWidth * rpx),
+          tabs: <Widget>[
+            Text('男生频道'),
+            Text('女生频道'),
+          ],
+        ),
+      ),
+      body: _tabViewWidget(),
+    );
+  }
+}
+
+class SortData {
+  String sortName;
+  List sortList = [];
+
+  SortData({this.sortName, this.sortList});
+}
+
+class BookDetail {
+  int bookId = 1;
+  String bookImg = 'images/OIP.jpg';
+  String bookTitle = '无题';
+  String author = '佚名';
+  String categoryName = '传说';
+  String bookDesc =
+      '是十九世纪和手机号就是时间合适就合适是十九世纪和手机号就是时间合适就合适是十九世纪和手机号就是时间合适就合适是十九世纪和手机号就是时间合适就合适';
+}
+
+class SortListData extends SortData {
+  BookDetail data = BookDetail();
+
+  @override
+  List get sortList => List.generate(5, (index) => sortList..add(data));
+  @override
+  String get sortName => sortName = '人气榜';
 }
