179b32daa01f946feedb551fae2a18df1a8414c6
HackerDaKun007 <dakun007@hotmail.com>
2020-05-21 01:01:38 +0800

    更新路由

--- lib/route/Route.dart
+++ lib/route/Route.dart
@@ -1,5 +1,4 @@
-import 'package:fiction/app/book_list/book_list_detail.dart';
-import 'package:fiction/app/book_list/book_list_page.dart';
+
 /**
  * @author      HackerDaKun007 <hackerdakun007@gmail.com>
  * @Remarks     无
@@ -13,10 +12,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';  //动画执行
 
 //大坤配置_路由区域
+// import 'package:fiction/app/Text.dart';  //测试
 import 'package:fiction/app/tabs/Tabs.dart';  //首页底部导航栏目
 import 'package:fiction/app/pages/home/Home.dart';  //书城
 import 'package:fiction/app/pages/bookshelf/BookShelf.dart'; //书架
 import 'package:fiction/app/pages/bookshelf/Recording.dart';  //阅读记录
+import 'package:fiction/app/pages/bookshelf/EditBookShelf.dart';  //编辑书架
 import 'package:fiction/app/search/Search.dart';  //搜索页面
 import 'package:fiction/app/search/Result.dart';  //搜索结果页面
 
@@ -25,13 +26,17 @@ import 'package:fiction/app/pages/My.dart';  //我的
 
 //Yuri配置_路由区域
 import 'package:fiction/app/pages/home/top_charts.dart'; // 排行榜
+import 'package:fiction/app/book_list/book_list_detail.dart';
+import 'package:fiction/app/book_list/book_list_page.dart';
 
 //配置命令路由
 final routes = {
+  // '/text': (context) => MyHomePage(),
   '/': (context) => Tabs(),
   '/home': (context) => HomePage(),
   '/bookshelf': (context) => BookShelfPage(),
   '/recording': (context) => Recording(),
+  '/editbookshelf': (context) => EditBookShelf(),
 
   '/search': (context) => Search(),
   '/result': (context, {arguments}) => ResultSearch(arguments:arguments),
