84bbf2cddd673a91698c9cb719f596fdeab5c2e0
HackerDaKun007 <dakun007@hotmail.com>
2020-05-27 11:32:27 +0800

    网络请求封装页面完成

--- lib/public/widget/internet.dart
+++ lib/public/widget/internet.dart
@@ -10,13 +10,18 @@
 import 'package:flutter/material.dart';
 import 'package:fiction/public/public.dart';
 import 'package:connectivity/connectivity.dart';
+import 'package:fiction/public/utils/bounced.dart';
 
 class Internet extends StatefulWidget {
   Widget centent;
-  Internet(this.centent, {Key key}) : super(key: key);
+
+  //加载动画
+  String value; //提示文本
+  bool isLoad;
+  Internet(this.centent, this.isLoad, {Key key, this.value:'加载中...'}) : super(key: key);
 
   @override
-  _InternetState createState() => _InternetState(centent=this.centent);
+  _InternetState createState() => _InternetState(centent=this.centent, isLoad=this.isLoad,value:this.value);
 }
 
 class _InternetState extends State<Internet> with PixelSize {
@@ -26,7 +31,12 @@ class _InternetState extends State<Internet> with PixelSize {
   bool isInternet = true;
   Widget centent; //内容值
 
-  _InternetState(this.centent);
+  var bounced = Bounced(); //请求的值
+
+  //加载动画
+  String value; //提示文本
+  bool isLoad;
+  _InternetState(this.centent, this.isLoad, {this.value});
 
   @override
   dispose() {
@@ -50,6 +60,7 @@ class _InternetState extends State<Internet> with PixelSize {
           isInternet = true;
         });
       } else {
+        bounced.alert('提示', '您当前网络存在异常', context);
         setState(() {
           stateText = "none";
           isInternet = false;
@@ -59,13 +70,19 @@ class _InternetState extends State<Internet> with PixelSize {
   }
 
   Widget build(BuildContext context) {
-    return init(this.centent);
+    return _init(this.centent);
   }
 
-  Widget init(Widget centent) {
+  //初始化定义数据
+  Widget _init(Widget centent) {
     //判断网络 true网络正常，false网站不正常
     if (isInternet) {
-      return centent;
+      //isLoad: true返回正常内容， false返回加载内容
+      if (isLoad) {
+        return centent;
+      } else {
+        return _load(this.value);
+      }
     } else {
       return Center(
         child: Column(
@@ -87,5 +104,40 @@ class _InternetState extends State<Internet> with PixelSize {
         ),
       );
     }
+  }
+
+  //加载动画
+  Widget _load(String value) {
+    return Center(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Container(
+            width: getPixe(120, context),
+            height: getPixe(120, context),
+            // padding: EdgeInsets.all(getPixe(20, context),
+            decoration: BoxDecoration(
+              color: Colors.black38,
+              borderRadius: BorderRadius.circular(getPixe(4, context)),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(
+                  // strokeWidth: 1.0,
+                  // backgroundColor: Colors.black38,
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                  // backgroundColor: Colors.black,
+                ),
+                SizedBox(height: getPixe(15, context),),
+                 Text(
+                value,
+                style: TextStyle(fontSize: 16.0, color:Colors.white),
+              )
+              ],
+            ),
+          ),
+        ),
+      );
   }
 }
