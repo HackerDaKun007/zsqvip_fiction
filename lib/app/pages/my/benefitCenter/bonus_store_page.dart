a0f692e062f0dc03f64677895c5477990f63cd21
401ZZ <pluto401zz@gmail.com>
2020-06-03 14:37:19 +0800

    调整界面布局

--- lib/app/pages/my/benefitCenter/bonus_store_page.dart
+++ lib/app/pages/my/benefitCenter/bonus_store_page.dart
@@ -34,82 +34,86 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
       decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('images/welfare.jpg'), fit: BoxFit.cover)),
-      child: Container(
-        padding: EdgeInsets.only(top: getPixe(35, context)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: isLogin
-              ? <Widget>[
-                  Text(
-                    '当前积分',
-                    style: TextStyle(
-                        fontSize: getPixe(12, context), color: Colors.white54),
+      child: _buildHeaderText(),
+    );
+  }
+
+  Widget _buildHeaderText() {
+    return Container(
+      padding: EdgeInsets.only(top: getPixe(35, context)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: isLogin
+            ? <Widget>[
+                Text(
+                  '当前积分',
+                  style: TextStyle(
+                      fontSize: getPixe(12, context), color: Colors.white54),
+                ),
+                SizedBox(
+                  height: getPixe(10, context),
+                ),
+                Text(
+                  '50',
+                  style: TextStyle(
+                    fontSize: getPixe(45, context),
+                    color: Colors.white,
                   ),
-                  SizedBox(
-                    height: getPixe(10, context),
-                  ),
-                  Text(
-                    '50',
-                    style: TextStyle(
-                      fontSize: getPixe(45, context),
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(
-                    height: getPixe(8, context),
-                  ),
-                  InkWell(
-                    onTap: toBack,
-                    child: Container(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: getPixe(50, context),
-                            vertical: getPixe(6, context)),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.circular(getPixe(20, context)),
-                            gradient: LinearGradient(
-                                colors: [Color(0xffE3B967), Color(0xffF8E1AA)],
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight)),
-                        child: Text(
-                          '去赚积分>',
-                          style: TextStyle(
-                              fontSize: getPixe(15, context),
-                              fontWeight: FontWeight.w600,
-                              color: Colors.deepOrange[900]),
-                        )),
-                  )
-                ]
-              : <Widget>[
-                  Text(
-                    '登录可领取积分、兑换大奖',
-                    style: TextStyle(
-                        color: Colors.white, fontSize: getPixe(12, context)),
-                  ),
-                  SizedBox(
-                    height: getPixe(15, context),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Container(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: getPixe(35, context),
-                            vertical: getPixe(6, context)),
-                        decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                                colors: [Colors.blue[600], Colors.blue[300]],
-                                begin: Alignment.centerLeft,
-                                end: Alignment.centerRight),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Text(
-                          '立即登录',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: getPixe(14, context)),
-                        )),
-                  )
-                ],
-        ),
+                ),
+                SizedBox(
+                  height: getPixe(8, context),
+                ),
+                InkWell(
+                  onTap: toBack,
+                  child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: getPixe(50, context),
+                          vertical: getPixe(6, context)),
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(getPixe(20, context)),
+                          gradient: LinearGradient(
+                              colors: [Color(0xffE3B967), Color(0xffF8E1AA)],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight)),
+                      child: Text(
+                        '去赚积分>',
+                        style: TextStyle(
+                            fontSize: getPixe(15, context),
+                            fontWeight: FontWeight.w600,
+                            color: Colors.deepOrange[900]),
+                      )),
+                )
+              ]
+            : <Widget>[
+                Text(
+                  '登录可领取积分、兑换大奖',
+                  style: TextStyle(
+                      color: Colors.white, fontSize: getPixe(12, context)),
+                ),
+                SizedBox(
+                  height: getPixe(15, context),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: getPixe(35, context),
+                          vertical: getPixe(6, context)),
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.blue[600], Colors.blue[300]],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight),
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Text(
+                        '立即登录',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: getPixe(14, context)),
+                      )),
+                )
+              ],
       ),
     );
   }
@@ -117,7 +121,7 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
   /// 积分商城展示
   Widget _buildBonusDetail() {
     double mainWidth = getWidth(context);
-    double childWidth = getPixe((mainWidth - 53) / 3, context);
+    double childWidth = getPixe((mainWidth - 63) / 3, context);
     return Container(
       padding: EdgeInsets.all(getPixe(15, context)),
       width: mainWidth,
@@ -149,15 +153,18 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
                 ],
               ),
               Padding(
-                padding: EdgeInsets.fromLTRB(0, getPixe(15, context),
-                    getPixe(10, context), getPixe(10, context)),
+                padding: EdgeInsets.fromLTRB(
+                    0, getPixe(15, context), 0, getPixe(10, context)),
                 child: Wrap(
                   direction: Axis.horizontal,
                   spacing: getPixe(15, context),
+                  runSpacing: getPixe(15, context),
                   children: [
                     _getProductCard(
                         childWidth, 'images/welfare_integral_1.png', 'iPhone X',
                         ishot: true),
+                    _getProductCard(childWidth, 'images/welfare_integral_2.png',
+                        'iPhone X 碎片'),
                     _getProductCard(childWidth, 'images/welfare_integral_2.png',
                         'iPhone X 碎片'),
                   ],
@@ -175,12 +182,12 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
                   fontWeight: FontWeight.w600, fontSize: getPixe(20, context)),
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(0, getPixe(15, context),
-                  getPixe(10, context), getPixe(10, context)),
+              padding: EdgeInsets.fromLTRB(
+                  0, getPixe(15, context), 0, getPixe(10, context)),
               child: Wrap(
                 direction: Axis.horizontal,
                 spacing: getPixe(15, context),
-                runSpacing: getPixe(30, context),
+                runSpacing: getPixe(15, context),
                 children: [
                   _getTextCard(childWidth,
                       isNew: true, time: '5分钟', earnNum: 10),
@@ -264,7 +271,7 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
                           style: TextStyle(fontSize: getPixe(12, context)),
                         )
                       ]),
-                      Spacer(),
+                  Spacer(),
                   InkWell(
                     onTap: toBack,
                     child: Container(
@@ -375,7 +382,7 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
       {bool ishot = false}) {
     return Stack(children: [
       Container(
-        height: getPixe(160, context),
+        height: getPixe(165, context),
         width: childWidth,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -445,50 +452,51 @@ class _BonusStorePageState extends State<BonusStorePage> with PixelSize {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            '积分兑换',
-            style: TextStyle(fontSize: getPixe(20, context)),
-          ),
-          centerTitle: true,
-          elevation: 1,
-          leading: IconButton(
-            icon: const Icon(Iconfont.zuo),
-            iconSize: getPixe(26, context),
-            onPressed: toBack,
-          ),
-          actions: <Widget>[
-            InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, '/bonusdetail');
-                },
-                child: Container(
-                    padding: EdgeInsets.only(
-                      right: getPixe(15, context),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '明细',
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: getPixe(16, context),
-                            fontWeight: FontWeight.w500),
-                        textAlign: TextAlign.center,
-                      ),
-                    ))),
-          ],
+      appBar: AppBar(
+        title: Text(
+          '积分兑换',
+          style: TextStyle(fontSize: getPixe(20, context)),
         ),
-        body: SingleChildScrollView(
-            child: Container(
-          height: 800,
-          child: Stack(children: [
-            _buildHeader(),
-            Positioned(
-              top: getPixe(200, context),
-              child:
-                  Column(children: [_buildBonusDetail(), _buildFooterInfo()]),
-            ),
-          ]),
-        )));
+        centerTitle: true,
+        elevation: 1,
+        leading: IconButton(
+          icon: const Icon(Iconfont.zuo),
+          iconSize: getPixe(26, context),
+          onPressed: toBack,
+        ),
+        actions: <Widget>[
+          InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, '/bonusdetail');
+              },
+              child: Container(
+                  padding: EdgeInsets.only(
+                    right: getPixe(15, context),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '明细',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: getPixe(16, context),
+                          fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.center,
+                    ),
+                  ))),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Stack(children: [
+          Container(
+            height: 1000,
+          ),
+          _buildHeader(),
+          Positioned(
+            top: getPixe(200, context),
+            child: Column(children: [_buildBonusDetail(), _buildFooterInfo()]),
+          ),
+        ]),
+      ),
+    );
   }
 }
