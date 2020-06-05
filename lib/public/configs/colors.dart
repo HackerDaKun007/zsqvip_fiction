74b417c1ff98a6a4056362be884cdf7e75053fb1
401ZZ <pluto401zz@gmail.com>
2020-06-05 16:06:01 +0800

    fix: 颜色调整

--- lib/public/configs/colors.dart
+++ lib/public/configs/colors.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ZFColors {
-  static Color primaryColor = Colors.blue[400]; // 主颜色
-  static Color textColor = Color(0xff603c18);  // 阅读字体颜色
-  static Color toolTextColor = Color(0xffb9610b); // 阅读菜单字体颜色
-  static Color paperColor = Color(0xffE3D5AE); // 阅读背景颜色
-  static Color toolBarColor = Color(0xffF0E3C0); // 阅读菜单背景颜色
-  static Color greyColor = Colors.grey; // 阅读菜单背景颜色
-  static Color orangeColor = Colors.orange[800];
+  final Color primaryColor = Colors.blue[400]; // 主颜色
+  final Color textColor = Color(0xff603c18);  // 阅读字体颜色
+  final Color toolTextColor = Color(0xffb9610b); // 阅读菜单字体颜色
+  final Color paperColor = Color(0xffE3D5AE); // 阅读背景颜色
+  final Color toolBarColor = Color(0xffF0E3C0); // 阅读菜单背景颜色
+  final Color greyColor = Colors.grey; 
+  final Color orangeColor = Colors.orange[800]; 
 }
